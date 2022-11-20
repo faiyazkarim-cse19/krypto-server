40d613c4b900a2ef0d0299b1e2b9fb57b2c03b05
sfk-aayan <190041116faiyaz@gmail.com>
2022-11-20 15:26:22 +0600

    Added requests for Exchanges and Charts

--- index.js
+++ index.js
@@ -15,6 +15,55 @@ const db = mysql.createConnection({
     database : 'krypto'
 });
 
+
+app.post('/create', (req, res) => {
+    const email = req.body.email;
+    const coin = req.body.coin;
+    const amount = req.body.amount;
+ 
+    db.query(
+        "INSERT INTO input (I_email, I_coin, I_amount) VALUES (?, ?, ?)",
+        [email, coin, amount], (err, result) => {
+            if (err) {
+                console.log(err)
+            }   else {
+                res.send("Values Inserted")
+            }
+        }
+    );
+})
+ 
+app.post("/transactions", (req, res) => {
+    const email = req.body.email
+
+    db.query("SELECT T_coin, T_amount FROM Transactions WHERE T_email= ?",[email], (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+});
+
+/*app.get('/transaction', (req, res) => {
+    const email = req.body.email 
+
+    db.query("SELECT T_coin, T_amount FROM transactions WHERE T_email = ?;", 
+    [email], (err, result) => {
+        if (err){
+            res.send({err: err})
+        }
+            if(result.length > 0)
+            {
+            res.send(result);
+            }
+            else
+            {
+              res.send({message: "Wrong email!"});  
+            }
+    })
+})*/
+
 app.post('/register', (req,res) => {
     const name = req.body.name
     const email = req.body.email 
@@ -51,5 +100,44 @@ app.post('/login', (req, res) => {
             }
     })
 })
+
+app.post('/postinfo', (req, res) => {
+    const coinname = req.body.name 
+    const choice = req.body.choice
+
+    db.query("SELECT price,time FROM chartinfo WHERE coinName = ? AND choice = ?;", 
+    [coinname, choice], (err, result) => {
+        if (err){
+            res.send({err: err})
+        }
+            if(result.length > 0)
+            {
+            res.send(result);
+            }
+            else
+            {
+              res.send({message: "No Information in Database"});  
+            }
+    })
+})
+
+
+app.post('/info', (req,res) => {
+    const coinname = req.body.name
+    const coinprice = req.body.price 
+    const cointime = req.body.time
+    const choice = req.body.choice
+
+    db.query("INSERT INTO chartinfo (coinName, price, time, choice) VALUES (?,?,?,?);", 
+    [coinname, coinprice, cointime, choice], (err, result) => {
+        if (err){
+            console.log(err)
+        }
+        else
+        {
+            res.send(result)
+        }
+    })
+});
 
 app.listen(3001, () => {console.log("Successful Connection!")})
